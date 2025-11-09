dcae9ad64fff33ef70128731198100f86e62d75e
omkarshukla101 <omkarshukla.os2@gmail.com>
2025-11-10 00:24:09 +0530

    Create q5.cpp

---
+++ q5.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+bool isCircular(Node* head) {
+    if (!head) return false;
+    Node* temp = head->next;
+    while (temp && temp != head)
+        temp = temp->next;
+    return (temp == head);
+}
+
+int main() {
+    Node* head = new Node(2);
+    head->next = new Node(4);
+    head->next->next = new Node(6);
+    head->next->next->next = new Node(7);
+    head->next->next->next->next = new Node(5);
+    if (isCircular(head))
+        cout << "Circular Linked List";
+    else
+        cout << "Not Circular Linked List";
+}
